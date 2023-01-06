f3726bc7cefd077695791392dc81be907b0a6e28
Manjeet <mann3774@gmail.com>
2023-01-06 20:13:39 +0530

    Option to choose only audio or only video

--- dwnld_ytb_vd.py
+++ dwnld_ytb_vd.py
@@ -7,6 +7,8 @@ vd1 = YouTube(link)
 # print(vd1.title)
 
 streams = vd1.streams.all()
+# streams = vd1.streams.filter(only_audio= True)
+# streams = vd1.streams.filter(only_video= True)
 streamlist = list(enumerate(streams))
 for i in streamlist:
     print(i)
